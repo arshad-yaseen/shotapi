750f12bc14c01c8a6c97d9b63b77b2c313afab0d
arshad-yaseen <arshadpyaseen@gmail.com>
2023-12-26 02:30:53 +0530

    Fix timeout values and formatting in exceptions.py and test_api.py

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,4 +1,6 @@
 class MissingCloudinaryCredentialsError(ValueError):
     def __init__(self, missing_field):
-        message = f"{missing_field} is required when 'format' is 'cloudinary_url'"
+        message = (
+            f"{missing_field} is required when 'format' is 'cloudinary_url'"
+        )
         super().__init__(message)
