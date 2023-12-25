d56ceb2df24b2d31cb67740aa0f92f31b08cb972
arshad-yaseen <arshadpyaseen@gmail.com>
2023-12-25 23:42:58 +0530

    Selenium wait for page load

--- app/services/screenshot_taker.py
+++ app/services/screenshot_taker.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 async def take_screenshot(url: str):
     options = Options()
     options.add_argument('--no-sandbox')
+    # Standard laptop resolution
     options.add_argument('--window-size=1280,800')
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
